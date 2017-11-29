f1fce8b8af92c80c6144cce8f9ab9cde4b893afb
arbennett <bennett.andr@gmail.com>
2017-11-28 20:03:33 -0800

    Update app framework.

--- futurefish/dashboard.py
+++ futurefish/dashboard.py
@@ -2,37 +2,39 @@
 import dash_core_components as dcc
 import dash_html_components as html
 
+APP_STYLE = {}
 FULL_WIDTH = {'width': '100%'}
-LEFT_JUSTIFY = {'float': 'left'}
-RIGHT_JUSTIFY = {'float': 'right'}
+LEFT_JUSTIFY = {'width': '30%', 'float': 'left'}
+RIGHT_JUSTIFY = {'width': '30%', 'float': 'right'}
 
 
 def initialize_layout():
     header_elements = [
-            html.H1(children='Fish Finder'),
+            html.Img(src='https://lh6.googleusercontent.com/pKPrVVjUdvNcXEz0E-KxbCfTXMPuc301Db6YFM95Njt7HQTb0j4a3-NyT3na8SaBotSpAHOnu0mLky1QtB9u=w2560-h1374',
+                style={'display': 'block', 'margin': 'auto'}),
             html.Hr()
             ]
-    header = html.Div(header_elements, style=FULL_WIDTH)
+    header = html.Div(header_elements, className='header', style=FULL_WIDTH)
 
     info_elements = [
             html.H1(children='Information'),
             html.Hr()
             ]
-    information = html.Div(info_elements)
+    information = html.Div(info_elements, className='information_panel')
     selector_elements = [
             html.H1(children='Selector stuff'),
             html.Hr()
             ]
-    selector = html.Div(selector_elements)
+    selector = html.Div(selector_elements, className='selector')
     left_pane = html.Div([information, selector],
-                         style=LEFT_JUSTIFY)
+                         className='column_left')
 
     map_elements = [
             html.H1(children='Map element'),
             html.Hr()
             ]
     mapper = html.Div(map_elements)
-    right_pane = html.Div([mapper], style=RIGHT_JUSTIFY)
+    right_pane = html.Div([mapper], className='column_right')
 
-    base_layout = html.Div([header, left_pane, right_pane])
+    base_layout = html.Div([header, left_pane, right_pane], className='futurefish', style=APP_STYLE)
     return base_layout
--- scripts/futurefish_dash.py
+++ scripts/futurefish_dash.py
@@ -1,8 +1,7 @@
 import dash
-import futurefish as ff
-
+import futurefish
 
 if __name__ == '__main__':
     app = dash.Dash('Future Fish')
-    app.layout = ff.dashboard.initialize_layout()
+    app.layout = futurefish.dashboard.initialize_layout()
     app.run_server()
