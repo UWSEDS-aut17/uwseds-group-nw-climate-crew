12582c95f999168c94d5fd20fb70ca1590a38a37
arbennett <bennett.andr@gmail.com>
2017-12-05 19:45:26 -0800

    Update structure.

--- futurefish/dashboard.py
+++ futurefish/dashboard.py
@@ -1,9 +1,9 @@
 
 import os
 import base64
+import futurefish.futurefish as ff
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
 
 
 APP_STYLE = {}
@@ -11,20 +11,9 @@ FULL_WIDTH = {'width': '100%'}
 LEFT_JUSTIFY = {'width': '30%', 'float': 'left'}
 RIGHT_JUSTIFY = {'width': '30%', 'float': 'right'}
 
-DATA_STR = '~/Documents/CSE583_SEDS/climate-project/futurefish/data/tiny_site_test_dataset.csv'
-DATA = pd.read_csv(DATA_STR)
-TOKEN = 'pk.eyJ1IjoibWticmVubmFuIiwiYSI6ImNqYW12OGxjYjM1MXUzM28yMXhpdWE3NW0ifQ.Elj' \
-        'NVtky3qEFfvJL80RgMQ'
+INFORMATION = """
+"""
 
-MAP_HEIGHT = 500
-MAP_WIDTH = 700
-MAP_MARGIN = dict(t=0, b=0, l=0, r=0)
-MAP_FONT = dict(color='#FFFFFF', size=11)
-MAP_BG_COLOR = '#50667f'
-MAP_BEARING = 0
-MAP_CENTER = dict(lat=46, lon=-119)
-MAP_PITCH = 0
-MAP_ZOOM = 4.5
 
 def initialize_layout():
     logo_file = 'resources/images/logo_3.png'
@@ -66,29 +55,14 @@ def initialize_layout():
     return base_layout
 
 
-def make_colorscale(scl0 = 'rgb(0, 102, 0)', scl20 = 'rgb(128, 255, 0)', scl40 = 'rgb(255, 255, 51)',
-                    scl60 = 'rgb(255, 153, 51)', scl80 = 'rgb(255, 6, 6)'):
-    """Generates a discrete color scale for the map graphic.
-    @param scl0 - color string for the minimum value
-    @param scl20 - color string for the 20% value
-    @param scl40 - color string for the 40% value
-    @param scl60 - color string for the 60% value
-    @param scl80 - color string for the 80% value
-    @return 2D array containing scale-color pairings
-    """
-    return [[0, scl0], [0.2, scl0], [0.2, scl20], [0.4, scl20], [0.4, scl40], [0.6, scl40],
-            [0.6, scl60], [0.8, scl60], [0.8, scl80], [1.0, scl80]]
-
-
 def make_species_dropdown():
     """Generates a dropdown bar to select fish species.
     @return dash.dcc Dropdown object
     """
-    species = DATA['Species'].unique()
+    species = ff.DATA['Species'].unique()
     dd = dcc.Dropdown(
-        id='species',
+        id='species', value='A',
         options=[{'label': i, 'value': i} for i in species],
-        value='A'
     )
     return dd
 
@@ -97,11 +71,10 @@ def make_decade_radio():
     """Generates radio buttions for selecting the decade for which to display data.
     @return dash.dcc RadioItems object
     """
-    decades = DATA['Decade'].unique()
+    decades = ff.DATA['Decade'].unique()
     radio = dcc.RadioItems(
-        id='decade',
+        id='decade', value='2030-2059',
         options=[{'label': i, 'value': i} for i in decades],
-        value='2030-2059',
-        labelStyle={'width': '30%','display': 'inline-block'}
+        labelStyle={'width': '30%', 'display': 'inline-block'}
     )
     return radio
