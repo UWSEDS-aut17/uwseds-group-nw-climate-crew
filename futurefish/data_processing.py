b4bf650b0550078d065a45057f4ade5ae264e008
Katie Brennan <mkb22@uw.edu>
2017-12-13 22:23:53 -0800

    PEP8 fishmod.py, data_processing.py

--- futurefish/data_processing.py
+++ futurefish/data_processing.py
@@ -9,25 +9,25 @@ def calcLatLon(northing, easting):
 
     # CONSUS Albers variables (EPSG: 5070)
     RE_NAD83 = 6378137.0
-    E_NAD83 = 0.0818187034  #Eccentricity
-    D2R = 0.01745329251     #Pi/180
+    E_NAD83 = 0.0818187034  # Eccentricity
+    D2R = 0.01745329251     # Pi/180
     standardParallel1 = 43.
     standardParallel2 = 47.
     centralMeridian = -114.
     originLat = 30
     originLon = 0
 
-    m1 = cos(standardParallel1 * D2R)/ \
-         sqrt(1.0 - pow((E_NAD83 * sin(standardParallel1 * D2R)), 2.0))
-    m2 = cos(standardParallel2 * D2R)/ \
-         sqrt(1.0 - pow((E_NAD83 * sin(standardParallel2 * D2R)), 2.0))
+    m1 = cos(standardParallel1 * D2R) / \
+        sqrt(1.0 - pow((E_NAD83 * sin(standardParallel1 * D2R)), 2.0))
+    m2 = cos(standardParallel2 * D2R) / \
+        sqrt(1.0 - pow((E_NAD83 * sin(standardParallel2 * D2R)), 2.0))
 
     def calcPhi(i):
         sinPhi = sin(i * D2R)
-        return (1.0 - pow(E_NAD83, 2.0)) \
-               * ((sinPhi/(1.0 - pow((E_NAD83 * sinPhi), 2.0))) \
-               - 1.0/(2.0 * E_NAD83) \
-               * log((1.0 - E_NAD83 * sinPhi)/(1.0 + E_NAD83 * sinPhi)))
+        return (1.0 - pow(E_NAD83, 2.0)) * \
+               ((sinPhi/(1.0 - pow((E_NAD83 * sinPhi), 2.0))) -
+                1.0/(2.0 * E_NAD83) *
+                log((1.0 - E_NAD83 * sinPhi)/(1.0 + E_NAD83 * sinPhi)))
 
     q0 = calcPhi(originLat)
     q1 = calcPhi(standardParallel1)
@@ -37,13 +37,16 @@ def calcLatLon(northing, easting):
     rho0 = RE_NAD83 * sqrt(C - nc * q0) / nc
     rho = sqrt(pow(easting, 2.0) + pow((rho0 - northing), 2.0))
     q = (C - pow((rho * nc / RE_NAD83), 2.0)) / nc
-    beta = asin(q / (1.0 - log((1.0 - E_NAD83) / (1.0 + E_NAD83)) * (1.0 - pow(E_NAD83, 2.0))/(2.0 * E_NAD83)))
-    a = 1.0 / 3.0 * pow(E_NAD83, 2.0) + 31.0 / 180.0 * pow(E_NAD83, 4.0) + 517.0 / 5040.0 * pow(E_NAD83, 6.0)
+    beta = asin(q / (1.0 - log((1.0 - E_NAD83) / (1.0 + E_NAD83)) *
+                     (1.0 - pow(E_NAD83, 2.0))/(2.0 * E_NAD83)))
+    a = 1.0 / 3.0 * pow(E_NAD83, 2.0) + 31.0 / 180.0 * \
+        pow(E_NAD83, 4.0) + 517.0 / 5040.0 * pow(E_NAD83, 6.0)
     b = 23.0/360.0 * pow(E_NAD83, 4.0) + 251.0 / 3780.0 * pow(E_NAD83, 6.0)
     c = 761.0/45360.0 * pow(E_NAD83, 6.0)
     theta = atan2(easting, (rho0 - northing))
 
-    lat = (beta + a * sin(2.0 * beta) + b * sin(4.0 * beta) + c * sin(6.0 * beta))/D2R
+    lat = (beta + a * sin(2.0 * beta) + b * sin(4.0 * beta) +
+           c * sin(6.0 * beta))/D2R
     lon = centralMeridian + (theta / D2R) / nc
     coords = [lat, lon]
 
--- futurefish/model/fishmod.py
+++ futurefish/model/fishmod.py
@@ -7,14 +7,18 @@ path = "../data/"
 
 data_wea = pd.read_csv(path + stream_temp_file)
 data_latlon = pd.read_csv(path + site_info_file)
-data_latlon = data_latlon.iloc[:,0:4]
+data_latlon = data_latlon.iloc[:, 0:4]
 
-#data_wea = pd.read_csv("sites_streamflow_stream_temperature.csv")
-#data_latlon = pd.read_csv("full_site_test_dataset.csv")
+data_wea00 = data_wea.ix[:, ["Site ID",
+                             "Stream Temperature Historical",
+                             "Streamflow Historical"]]
+data_wea40 = data_wea.ix[:, ["Site ID",
+                             "Stream Temperature 2040s",
+                             "Streamflow 2040s"]]
+data_wea80 = data_wea.ix[:, ["Site ID",
+                             "Stream Temperature 2080s",
+                             "Streamflow 2080s"]]
 
-data_wea00 = data_wea.ix[:,["Site ID", "Stream Temperature Historical", "Streamflow Historical"]]
-data_wea40 = data_wea.ix[:,["Site ID", "Stream Temperature 2040s", "Streamflow 2040s"]]
-data_wea80 = data_wea.ix[:,["Site ID", "Stream Temperature 2080s", "Streamflow 2080s"]]
 data_wea00 = data_wea00.assign(Decade='1993-2005')
 data_wea40 = data_wea40.assign(Decade='2030-2059')
 data_wea80 = data_wea80.assign(Decade='2070-2099')
@@ -27,56 +31,127 @@ data_wea00 = pd.merge(data_latlon, data_wea00)
 data_wea40 = pd.merge(data_latlon, data_wea40)
 data_wea80 = pd.merge(data_latlon, data_wea80)
 
-### Temperature Model
+# Temperature Model
 # Streamt temperature historical
 temp = data_wea00["Temperature"]
-temp_vulnerability00 = fishmod_utils.temp_mod(temp, r_max=4, t_max=30, t_min=-5, t_opt=17.6)
+temp_vulnerability00 = fishmod_utils.temp_mod(temp,
+                                              r_max=4,
+                                              t_max=30,
+                                              t_min=-5,
+                                              t_opt=17.6)
 # Stream temperature 2040s
 temp = data_wea40["Temperature"]
-temp_vulnerability40 = fishmod_utils.temp_mod(temp, r_max=4, t_max=30, t_min=-5, t_opt=17.6)
+temp_vulnerability40 = fishmod_utils.temp_mod(temp,
+                                              r_max=4,
+                                              t_max=30,
+                                              t_min=-5,
+                                              t_opt=17.6)
 # Stream temperature 2080s
 temp = data_wea80["Temperature"]
-temp_vulnerability80 = fishmod_utils.temp_mod(temp, r_max=4, t_max=30, t_min=-5, t_opt=17.6)
+temp_vulnerability80 = fishmod_utils.temp_mod(temp,
+                                              r_max=4,
+                                              t_max=30,
+                                              t_min=-5,
+                                              t_opt=17.6)
 
-### Streamflow Model
+# Streamflow Model
+
 # Historical
 flow = data_wea00["Streamflow"]
-flow_vulnerability00_chinook = fishmod_utils.stream_mod(flow, fo=424, a=12818, b=424) # Chinook
-flow_vulnerability00_sockeye = fishmod_utils.stream_mod(flow, fo=390, a=946, b=390) # Sockeye
-flow_vulnerability00_coho = fishmod_utils.stream_mod(flow, fo=362, a=12124, b=362) # Coho
-flow_vulnerability00_pink = fishmod_utils.stream_mod_pink(flow, a_max=1052, f_max=1200, f_min=0, f_opt=420) # Pink
+flow_vulnerability00_chinook = fishmod_utils.stream_mod(flow,
+                                                        fo=424,
+                                                        a=12818,
+                                                        b=424)    # Chinook
+flow_vulnerability00_sockeye = fishmod_utils.stream_mod(flow,
+                                                        fo=390,
+                                                        a=946,
+                                                        b=390)    # Sockeye
+flow_vulnerability00_coho = fishmod_utils.stream_mod(flow,
+                                                     fo=362,
+                                                     a=12124,
+                                                     b=362)       # Coho
+flow_vulnerability00_pink = fishmod_utils.stream_mod_pink(flow,
+                                                          a_max=1052,
+                                                          f_max=1200,
+                                                          f_min=0,
+                                                          f_opt=420)  # Pink
+
 # Stream flow 2040s
 flow = data_wea40["Streamflow"]
-flow_vulnerability40_chinook = fishmod_utils.stream_mod(flow, fo=424, a=12818, b=424) # Chinook
-flow_vulnerability40_sockeye = fishmod_utils.stream_mod(flow, fo=390, a=946, b=390) # Sockeye
-flow_vulnerability40_coho = fishmod_utils.stream_mod(flow, fo=362, a=12124, b=362) # Coho
-flow_vulnerability40_pink = fishmod_utils.stream_mod_pink(flow, a_max=1052, f_max=1200, f_min=0, f_opt=420) # Pink
+flow_vulnerability40_chinook = fishmod_utils.stream_mod(flow,
+                                                        fo=424,
+                                                        a=12818,
+                                                        b=424)        # Chinook
+flow_vulnerability40_sockeye = fishmod_utils.stream_mod(flow,
+                                                        fo=390,
+                                                        a=946,
+                                                        b=390)        # Sockeye
+flow_vulnerability40_coho = fishmod_utils.stream_mod(flow,
+                                                     fo=362,
+                                                     a=12124,
+                                                     b=362)           # Coho
+flow_vulnerability40_pink = fishmod_utils.stream_mod_pink(flow,
+                                                          a_max=1052,
+                                                          f_max=1200,
+                                                          f_min=0,
+                                                          f_opt=420)  # Pink
+
 # stream flow 2080s
 flow = data_wea80["Streamflow"]
-flow_vulnerability80_chinook = fishmod_utils.stream_mod(flow, fo=424, a=12818, b=424) # Chinook
-flow_vulnerability80_sockeye = fishmod_utils.stream_mod(flow, fo=390, a=946, b=390) # Sockeye
-flow_vulnerability80_coho = fishmod_utils.stream_mod(flow, fo=362, a=12124, b=362) # Coho
-flow_vulnerability80_pink = fishmod_utils.stream_mod_pink(flow, a_max=1052, f_max=1200, f_min=0, f_opt=420) # Pink
+flow_vulnerability80_chinook = fishmod_utils.stream_mod(flow,
+                                                        fo=424,
+                                                        a=12818,
+                                                        b=424)        # Chinook
+flow_vulnerability80_sockeye = fishmod_utils.stream_mod(flow,
+                                                        fo=390,
+                                                        a=946,
+                                                        b=390)        # Sockeye
+flow_vulnerability80_coho = fishmod_utils.stream_mod(flow,
+                                                     fo=362,
+                                                     a=12124,
+                                                     b=362)           # Coho
+flow_vulnerability80_pink = fishmod_utils.stream_mod_pink(flow,
+                                                          a_max=1052,
+                                                          f_max=1200,
+                                                          f_min=0,
+                                                          f_opt=420)  # Pink
 
-### Combining temperature & streamflow vulnerability projections, assigning equal weight to each output
+# Combining temperature & streamflow vulnerability projections
+# assigning equal weight to each output
+
 # Historical
-chinook_vulnerability00 = pd.DataFrame(round(temp_vulnerability00*0.5 + flow_vulnerability00_chinook*0.5)) # Chinook
-sockeye_vulnerability00 = pd.DataFrame(round(temp_vulnerability00*0.5 + flow_vulnerability00_sockeye*0.5)) # Sockeye
-coho_vulnerability00 = pd.DataFrame(round(temp_vulnerability00*0.5 + flow_vulnerability00_coho*0.5)) # Coho
-pink_vulnerability00 = pd.DataFrame(round(temp_vulnerability00*0.5 + flow_vulnerability00_pink*0.5)) # Pink
-# Year 2040s
-chinook_vulnerability40 = pd.DataFrame(round(temp_vulnerability40*0.5 + flow_vulnerability40_chinook*0.5)) # Chinook
-sockeye_vulnerability40 = pd.DataFrame(round(temp_vulnerability40*0.5 + flow_vulnerability40_sockeye*0.5)) # Sockeye
-coho_vulnerability40 = pd.DataFrame(round(temp_vulnerability40*0.5 + flow_vulnerability40_coho*0.5)) # Coho
-pink_vulnerability40 = pd.DataFrame(round(temp_vulnerability40*0.5 + flow_vulnerability40_pink*0.5)) # Pink
-# Year 2080s
-chinook_vulnerability80 = pd.DataFrame(round(temp_vulnerability80*0.5 + flow_vulnerability80_chinook*0.5)) # Chinook
-sockeye_vulnerability80 = pd.DataFrame(round(temp_vulnerability80*0.5 + flow_vulnerability80_sockeye*0.5)) # Sockeye
-coho_vulnerability80 = pd.DataFrame(round(temp_vulnerability80*0.5 + flow_vulnerability80_coho*0.5)) # Coho
-pink_vulnerability80 = pd.DataFrame(round(temp_vulnerability80*0.5 + flow_vulnerability80_pink*0.5)) # Pinks
+chinook_vulnerability00 = pd.DataFrame(round(temp_vulnerability00*0.5 +
+                                             flow_vulnerability00_chinook*0.5))
+sockeye_vulnerability00 = pd.DataFrame(round(temp_vulnerability00*0.5 +
+                                             flow_vulnerability00_sockeye*0.5))
+coho_vulnerability00 = pd.DataFrame(round(temp_vulnerability00*0.5 +
+                                          flow_vulnerability00_coho*0.5))
+pink_vulnerability00 = pd.DataFrame(round(temp_vulnerability00*0.5 +
+                                          flow_vulnerability00_pink*0.5))
 
-### Combining all model outputs to a dataframe
+# Year 2040s
+chinook_vulnerability40 = pd.DataFrame(round(temp_vulnerability40*0.5 +
+                                             flow_vulnerability40_chinook*0.5))
+sockeye_vulnerability40 = pd.DataFrame(round(temp_vulnerability40*0.5 +
+                                             flow_vulnerability40_sockeye*0.5))
+coho_vulnerability40 = pd.DataFrame(round(temp_vulnerability40*0.5 +
+                                          flow_vulnerability40_coho*0.5))
+pink_vulnerability40 = pd.DataFrame(round(temp_vulnerability40*0.5 +
+                                          flow_vulnerability40_pink*0.5))
+
+# Year 2080s
+chinook_vulnerability80 = pd.DataFrame(round(temp_vulnerability80*0.5 +
+                                             flow_vulnerability80_chinook*0.5))
+sockeye_vulnerability80 = pd.DataFrame(round(temp_vulnerability80*0.5 +
+                                             flow_vulnerability80_sockeye*0.5))
+coho_vulnerability80 = pd.DataFrame(round(temp_vulnerability80*0.5 +
+                                          flow_vulnerability80_coho*0.5))
+pink_vulnerability80 = pd.DataFrame(round(temp_vulnerability80*0.5 +
+                                          flow_vulnerability80_pink*0.5))
+
+# Combining all model outputs to a dataframe
 # Make a sub-dataframe for each species - historical
+
 # Chinook
 chinook_00 = data_wea00
 chinook_00 = chinook_00.assign(Species='Chinook')
@@ -94,7 +169,8 @@ pink_00 = data_wea00
 pink_00 = pink_00.assign(Species='Pink')
 pink_00["Viability"] = pink_vulnerability00
 
-## make a sub-dataframe for each species - 40s
+# make a sub-dataframe for each species - 40s
+
 # Chinook
 chinook_40 = data_wea40
 chinook_40 = chinook_40.assign(Species='Chinook')
@@ -112,7 +188,8 @@ pink_40 = data_wea40
 pink_40 = pink_40.assign(Species='Pink')
 pink_40["Viability"] = pink_vulnerability40
 
-## make a sub-dataframe for each species - 80s
+# make a sub-dataframe for each species - 80s
+
 # Chinook
 chinook_80 = data_wea80
 chinook_80 = chinook_80.assign(Species='Chinook')
@@ -130,9 +207,17 @@ pink_80 = data_wea80
 pink_80 = pink_80.assign(Species='Pink')
 pink_80["Viability"] = pink_vulnerability80
 
-### Merging output
-fish_vulnerability_00 = chinook_00.append(sockeye_00).append(coho_00).append(pink_00)
-fish_vulnerability_40 = chinook_40.append(sockeye_40).append(coho_40).append(pink_40)
-fish_vulnerability_80 = chinook_80.append(sockeye_80).append(coho_80).append(pink_80)
-fish_vulnerability = fish_vulnerability_00.append(fish_vulnerability_40).append(fish_vulnerability_80)
+# Merging output
+fish_vulnerability_00 = chinook_00.append(sockeye_00). \
+                                   append(coho_00). \
+                                   append(pink_00)
+fish_vulnerability_40 = chinook_40.append(sockeye_40). \
+                                   append(coho_40). \
+                                   append(pink_40)
+fish_vulnerability_80 = chinook_80.append(sockeye_80). \
+                                   append(coho_80). \
+                                   append(pink_80)
+fish_vulnerability = fish_vulnerability_00. \
+                     append(fish_vulnerability_40). \
+                     append(fish_vulnerability_80)
 fish_vulnerability.to_csv("../data/fish_vulnerability_new.csv")
